3b0a5c2f8fb266ee0139751744bedfa1cc144b68
S. Suzuki <terrierscript@gmail.com>
2022-08-30 23:29:23 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import * as SqlString from "sqlstring"
+import * as Papa from "papaparse"
+
+const parseParam = (param: string) => {
+  const removeBlacket = param
+    .replace(/^\[/, "")
+    .replace(/\]$/, "")
+  const parsed = Papa.parse(removeBlacket, {
+    dynamicTyping: true
+  })
+  return parsed.data[0]
+}
+
+type PrismaQueryEventLike = {
+  timestamp: Date
+  query: string
+  params: string
+  duration: number
+  target: string
+}
+
+type PrismaClientLike = {
+  $queryRawUnsafe(query: string, ...values: any[]): Promise<any>
+}
+
+export const explainQuery = async (prisma: PrismaClientLike, event: PrismaQueryEventLike) => {
+  if (!event.query.startsWith("SELECT ")) {
+    return
+  }
+
+  const params = parseParam(event.params)
+
+  if (!Array.isArray(params)) {
+    return
+  }
+  const rawQuery = SqlString.format(event.query, params)
+
+  const explain = `EXPLAIN ${rawQuery}`
+  const explainResults = await prisma.$queryRawUnsafe(explain)
+  if (!Array.isArray(explainResults)) {
+    return []
+  }
+  return explainResults.map(row => {
+    return {
+      "id": row["f0"],
+      "select_type": row["f1"],
+      "table": row["f2"],
+      "partitions": row["f3"],
+      "type": row["f4"],
+      "possible_keys": row["f5"],
+      "key": row["f6"],
+      "key_len": row["f7"],
+      "ref": row["f8"],
+      "rows": row["f9"],
+      "filtered": row["f10"],
+      "extra": row["f11"]
+    }
+  })
+}
+
+export default explainQuery
---
+++ test/index.test.ts
@@ -0,0 +1,48 @@
+import { explainQuery } from '../src'
+
+describe('prisma-mysql-explain ', () => {
+  it('explainQuery', async () => {
+    const mockClientLike = {
+      $queryRawUnsafe: async () => {
+        return [
+          {
+            f0: 1n,
+            f1: 'SIMPLE',
+            f2: 'Post',
+            f3: null,
+            f4: 'ALL',
+            f5: null,
+            f6: null,
+            f7: null,
+            f8: null,
+            f9: 1n,
+            f10: 100,
+            f11: 'Using where'
+          }
+        ]
+      }
+    }
+    const mockEvent = {
+      timestamp: new Date(),
+      query: 'SELECT * FROM Post WHERE title = ?',
+      params: '["x"]',
+      duration: 66,
+      target: 'quaint::connector::metrics'
+    }
+    const result = await explainQuery(mockClientLike, mockEvent)
+    expect(result).toEqual([{
+      "id": 1n,
+      "key": null,
+      "key_len": null,
+      "partitions": null,
+      "possible_keys": null,
+      "ref": null,
+      "rows": 1n,
+      "select_type": "SIMPLE",
+      "table": "Post",
+      "type": "ALL",
+      "filtered": 100,
+      "extra": "Using where",
+    }])
+  })
+})
