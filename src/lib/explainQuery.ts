34f14d7087e87111c4b1bc664275261e65a45604
S. Suzuki <terrierscript@gmail.com>
2022-08-31 20:58:41 +0900

    remove file

--- src/lib/explainQuery.ts
+++
@@ -1,3 +0,0 @@
-import { convertExplainRecord } from "./convertExplainRecord"
-import { revertToRawQuery } from "./revertToRawQuery"
-import { PrismaClientLike, PrismaQueryEventLike } from "./types"
