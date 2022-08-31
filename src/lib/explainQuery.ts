cb93285f0f6ddd88ff203decf6c4f9e8e5fdb616
S. Suzuki <terrierscript@gmail.com>
2022-08-31 20:56:23 +0900

    split functions

---
+++ src/lib/explainQuery.ts
@@ -0,0 +1,3 @@
+import { convertExplainRecord } from "./convertExplainRecord"
+import { revertToRawQuery } from "./revertToRawQuery"
+import { PrismaClientLike, PrismaQueryEventLike } from "./types"
